cc001838ad93c4d0dfe7d9cc18487cc5ad6ee7c1
DenisRx <denis.roux@epitech.eu>
2024-05-11 18:46:19 +0200

    Initial commit

---
+++ src/main/java/com/paris2024/ticketing/Paris2024TicketingApplication.java
@@ -0,0 +1,13 @@
+package com.paris2024.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paris2024TicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paris2024TicketingApplication.class, args);
+	}
+
+}
