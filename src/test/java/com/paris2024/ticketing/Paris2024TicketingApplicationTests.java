3cf3447dd51a65c7c379c9a43d25f0f62155e1c4
DenisRx <denis.roux@epitech.eu>
2024-05-25 12:54:08 +0200

    Setup JUnit tests

--- src/test/java/com/paris2024/ticketing/Paris2024TicketingApplicationTests.java
+++ src/test/java/com/paris2024/ticketing/Paris2024TicketingApplicationTests.java
@@ -1,17 +1,27 @@
 package com.paris2024.ticketing;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
 
-@Import(SecurityConfig.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class Paris2024TicketingApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void accessDeniedPageGet() throws Exception {
+		mockMvc.perform(get("/403"))
+			.andExpect(status().isOk())
+			.andExpect(view().name("403"));
 	}
 
 }
